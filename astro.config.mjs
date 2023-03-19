13f92d2f71b084983f0c3c19932f729ab8e36b93
Serhiy <holiaka.s@phenomenon-studio.com>
2023-03-20 00:47:30 +0200

    add menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import image from "@astrojs/image";
-
-import react from "@astrojs/react";
+import image from '@astrojs/image';
+import react from '@astrojs/react';
+import astroI18next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()]
+  integrations: [tailwind(), image(), react(), astroI18next()],
 });
