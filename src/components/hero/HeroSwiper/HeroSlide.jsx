11923e730205a03197651a6fabdef879d10eab85
Serhiy <holiaka.s@phenomenon-studio.com>
2023-03-22 01:27:48 +0200

    404 issue

--- src/components/hero/HeroSwiper/HeroSlide.jsx
+++ src/components/hero/HeroSwiper/HeroSlide.jsx
@@ -7,7 +7,7 @@ const HeroSlide = ({ image, href, title }) => {
                 <h1 className="text-[60px] 2xl:text-[72px] leading-[70px] 2xl:leading-[85px] font-medium">{title}</h1>
                 <div className="py-10 inline-flex items-center"><img width={57} height={32} src="/images/icon/360_icon.svg" alt="360-icon"/><span className="text-xl ml-4">Virtual tour</span></div>
                 <div>
-                    <a href={href} className="py-[20px] px-[26px] bg-yellow inline-flex items-center rounded-lg border border-transparent hover:border-yellow hover:bg-yellow-200 active:bg-yellow [&>svg]:hover:transform [&>svg]:hover:translate-x-[6px] transition-colors duration-200"><span>Find out the cost of a salt room</span><ArrowIcon className="ml-4 [&>path]:stroke-black-light transition-transform duration-200" /></a>
+                    <a href={href} className="text-base font-medium py-[20px] px-[26px] bg-yellow inline-flex items-center rounded-lg border border-transparent hover:border-yellow hover:bg-yellow-200 active:bg-yellow [&>svg]:hover:transform [&>svg]:hover:translate-x-[6px] transition-colors duration-200"><span>Find out the cost of a salt room</span><ArrowIcon className="ml-4 [&>path]:stroke-black-light transition-transform duration-200" /></a>
                 </div>
             </div>
             <div className="relative rounded-l-lg overflow-hidden">
--- src/components/hero/HeroSwiper/HeroSwiper.jsx
+++ src/components/hero/HeroSwiper/HeroSwiper.jsx
@@ -1,82 +1,90 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation, Pagination } from "swiper";
-import CallIcon from "../../ui/icons/CallIcon";
-import HeroSlider from "./HeroSlide";
-import { SLIDER_LIST } from "../../../constants/constants";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation, Pagination } from 'swiper';
+import CallIcon from '../../ui/icons/CallIcon';
+import HeroSlider from './HeroSlide';
+import { SLIDER_LIST } from '../../../constants/constants';
 import ArrowIcon from '../../ui/icons/ArrowIcon';
-import "swiper/css/autoplay";
-import "swiper/css/navigation";
-import "swiper/css/pagination"
+import 'swiper/css/autoplay';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const HeroSwiper = () => {
-    return (
-        <div className="relative w-full h-full">
-            <Swiper
-                modules={[Autoplay, Navigation, Pagination]}
-                slidesPerView={1}
-                autoplay={{
-                    delay: 5000,
-                }}
-                loop={true}
-                navigation={{
-                    prevEl: '#prev',
-                    nextEl: '#next',
-                }}
-                pagination={{
-                    clickable: true,
-                    el: "#pagination",
-                    bulletActiveClass: "actived",
-                    renderBullet: function (_, className) {
-                        return `<span class="${className} block w-1 h-1 bg-white mr-1 last:mr-0"></span>`;
-                      },
-                }}
-                className="h-full"
+  return (
+    <div className='relative w-full h-full'>
+      <Swiper
+        modules={[Autoplay, Navigation, Pagination]}
+        slidesPerView={1}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        navigation={{
+          prevEl: '#prev',
+          nextEl: '#next',
+        }}
+        pagination={{
+          clickable: true,
+          el: '#pagination',
+          bulletClass: 'bullet-custom',
+          bulletActiveClass: 'active-point',
+          renderBullet: function (_, className) {
+            return `<svg class="${className} mr-7 last:mr-0" height="10" width="10">
+            <circle cx="5" cy="5" r="4" stroke="#393939" stroke-width="1" fill-opacity="0" />
+        </svg>`;
+          },
+        }}
+        className='h-full'
+      >
+        {SLIDER_LIST.map(({ title, href, image }, i) => (
+          <SwiperSlide key={i}>
+            <HeroSlider title={title} href={href} image={image} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className='absolute bottom-0 left-0 w-full xl:w-3/4 flex justify-end bg-black-light h-[199px] xl:rounded-tr-xl z-10 overflow-hidden'>
+        <div className='flex items-center justify-between w-2/3 pl-[70px] pr-[125px]'>
+          <div className='flex'>
+            <button
+              id='prev'
+              className='flex items-center justify-center w-[36px] h-[36px] mr-4 rounded-full border border-[#393939] hover:border-yellow [&_path]:hover:stroke-white transition'
             >
-                {SLIDER_LIST.map(({ title, href, image }, i) => (
-                    <SwiperSlide key={i}>
-                        <HeroSlider title={title} href={href} image={image} />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-            <div className="absolute bottom-0 left-0 w-full xl:w-3/4 flex justify-end bg-black-light h-[199px] xl:rounded-tr-xl z-10 overflow-hidden">
-                <div className="flex items-center justify-between w-2/3 pl-[70px] pr-[125px]">
-                    <div className="flex">
-                        <button id="prev" className="flex items-center justify-center w-[36px] h-[36px] mr-4 rounded-full border border-[#393939] hover:border-yellow [&_path]:hover:stroke-white transition"><ArrowIcon className="[&>path]:stroke-[#393939] transition rotate-180" /></button>
-                        <button id="next" className="flex items-center justify-center w-[36px] h-[36px] ml-4 rounded-full border border-[#393939] hover:border-yellow [&_path]:hover:stroke-white transition"><ArrowIcon className="[&>path]:stroke-[#393939] transition" /></button>
-                    </div>
-                    <div id="pagination" className="!w-auto flex" />
-                </div>
-                <div className="flex flex-col justify-center w-2/5 xl:w-1/3 pl-[4%] pr-[1%] 2xl:pl-[8%] 2xl:pr-[2%] bg-black-dark h-full self-end">
-                    <h6 className="text-base text-gray-light font-medium">
-                        If you have any questions, call right now
-                    </h6>
-                    <div className="flex items-center mt-5">
-                        <div className="text-xl text-white font-bold">
-                            <p>
-                                <span className="text-sm text-[#7B8487]">
-                                    +38
-                                </span>{" "}
-                                (093) 833-29-99
-                            </p>
-                            <p>
-                                <span className="text-sm text-[#7B8487]">
-                                    +38
-                                </span>{" "}
-                                (066) 591-47-67
-                            </p>
-                            <p>
-                                <span className="text-sm text-[#7B8487]">
-                                    +38
-                                </span>{" "}
-                                (093) 652-05-62
-                            </p>
-                        </div>
-                        <CallIcon className="ml-[10%]" />
-                    </div>
-                </div>
-            </div>
+              <ArrowIcon className='[&>path]:stroke-[#393939] transition rotate-180' />
+            </button>
+            <button
+              id='next'
+              className='flex items-center justify-center w-[36px] h-[36px] ml-4 rounded-full border border-[#393939] hover:border-yellow [&_path]:hover:stroke-white transition'
+            >
+              <ArrowIcon className='[&>path]:stroke-[#393939] transition' />
+            </button>
+          </div>
+          <div id='pagination' className='!w-auto flex' />
         </div>
-    );
+        <div className='flex flex-col justify-center w-2/5 xl:w-1/3 pl-[4%] pr-[1%] 2xl:pl-[8%] 2xl:pr-[2%] bg-black-dark h-full self-end'>
+          <h6 className='text-base text-gray-light font-medium'>
+            If you have any questions, call right now
+          </h6>
+          <div className='flex items-center mt-5'>
+            <div className='text-xl text-white font-bold'>
+              <p>
+                <span className='text-sm text-[#7B8487]'>+38</span> (093)
+                833-29-99
+              </p>
+              <p>
+                <span className='text-sm text-[#7B8487]'>+38</span> (066)
+                591-47-67
+              </p>
+              <p>
+                <span className='text-sm text-[#7B8487]'>+38</span> (093)
+                652-05-62
+              </p>
+            </div>
+            <CallIcon className='ml-[10%]' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSwiper;
